18e374bc11446f1b447d0c6e8ffcad36027d8441
AdemMethlouthi <ademmathlouthi14@gmail.com>
2026-02-14 19:11:08 +0100

    signalement

---
+++ hack-for-hope-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SignalementForm from './SignalementForm';
+
+function App() {
+  return (
+    // Pas besoin de div supplémentaire, le composant gère sa structure
+    <SignalementForm />
+  );
+}
+
+export default App;
